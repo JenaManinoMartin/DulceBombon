cbbd1170e130d8e48bb94db38fc66bdbb9744183
JenaManinoMartin <jenamanino77@gmail.com>
2024-05-09 21:43:26 -0300

    Header y Footer completados, modulación con JS.

---
+++ js/footer.js
@@ -0,0 +1,28 @@
+let miFooter = `
+<div id="contenedor">
+            <div id="social">
+                <p><span>DULCE</span></p>
+                <p><span>BOMBÓN</span></p>
+                <div id="links">
+                    <a href="https://www.instagram.com/dulcebombonpasteleria?igsh=OWkxdjIzYTk3c3R0" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                </div>
+            </div>
+            <div id="help">
+                <h4>AYUDA</h4>
+                <a href="/templates/FaQ.html">Preguntas frecuentes</a>
+                <a href="/templates/terms-and-conditions.html">Terminos y condiciones</a>
+                <a href="">Contactanos</a>
+            </div>
+            <div id="info">
+                <h4>INFORMACION</h4>
+                <ul>
+                    <li id="location"><i class="fa-solid fa-location-dot"></i>Argentina, Mendoza</li>
+                    <li id="email"><i class="fa-solid fa-envelope"></i>dulcebombonpasteleria@gmail.com</li>
+                    <li id="phone"><i class="fa-solid fa-phone-volume"></i>54 9 261 390-1040</li>
+                </ul>
+            </div>
+        </div>
+`
+document.querySelector("footer").innerHTML = miFooter
---
+++ js/header.js
@@ -0,0 +1,14 @@
+let miHeader = `
+<div id="logo">
+    <a href="/">DULCE <span>BOMBÓN</span></a>
+</div>
+<nav>
+    <a href="/">INICIO</a>
+    <a href="">QUIENES SOMOS</a>
+    <a href="">PRODUCTOS</a>
+    <a href="">CONTACTO</a>
+    <a href=""><i class="fa-solid fa-bag-shopping"></i></a>
+</nav>
+<hr>
+`
+document.querySelector("header").innerHTML = miHeader
