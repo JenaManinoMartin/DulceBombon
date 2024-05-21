8b745282cfba633f394849a8f2dda8e9e1d2fa6f
sargoo <goon_aguera@hotmail.com>
2024-05-21 02:22:03 -0300

    finalizado elemento del carrito de compra

--- js/head.js
+++ js/head.js
@@ -23,6 +23,8 @@ let miHead = `
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8H288c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z"/></svg>
+
     <title>Dulce Bombón</title>
 `
 document.querySelector("head").innerHTML = miHead
