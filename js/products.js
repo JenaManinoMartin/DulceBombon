657eabcb62c3db9a656017e41ff7e3688974bd26
ngatti68 <norberto.o.gatti@gmail.com>
2024-05-21 22:02:31 -0300

    Ajustes del flex de la sección productos

--- js/products.js
+++ js/products.js
@@ -1,13 +1,15 @@
 let categoryProduct = ``
 
 for(let item of categoryPrime){
-    categoryProduct = categoryProduct + `
-    
+    categoryProduct = categoryProduct + `   
+
             <article class="card__item">  
                 <a class="card-item__link" href="category-detail.html">
                     <span class="card-item__span" >ver mas</span>
                    <picture class="card-item__cover">
+                   <div class="img">
                       <img src="${item.image}" alt="Figura categoria de producto Dulce Bombón de Macarons">
+                      </div>                      
                    </picture>
                    <div class="card-item__content">
                       <p class="card-item__title">${item.name.toUpperCase()}</p>
